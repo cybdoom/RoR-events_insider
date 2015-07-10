b37d96f35973a09a22bb650f5d1f89fc2d965abe
pineapple-thief <gluhov1985@gmail.com>
2015-07-11 02:47:27 +1000

    Heroku-related change for production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Needed to deploy on Heroku with Devise installed
+  config.assets.initialize_on_precompile = false
 end
