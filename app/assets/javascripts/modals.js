672866dd1969eda9012b37c87d0b5854ce9f6940
Essekia Paul <essekiarockz@gmail.com>
2015-07-28 12:54:10 +0530

    1. Coding standards: a. use space instead of tabulation b.  let's add extra rule: 'insert space between "if", "else" "else if" keywords and boolean condition’
    2. JS:
    a. Moving JS library to vendor/assets/javascripts and removing duplicate entries.
    b. I cannot find a reference in JQuery docs to suggest it is deprecated. But, I agree “JQuery.on” would be the better option. - done.
    c. JQuery selector caching. Used $_this instead of $this. ( because of the coding standards mention in airbnb js standards. ( should I revert ?)

--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -1,51 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var documentHeight = $(document).height();
+  $document = $(document);
+  var documentHeight = $document.height();
+  $datePickerContainer = $('.date-picker-container');
 
-	$(".date-picker-container").height(documentHeight);
-	$(document).click(function(event) {
-		var target = $(event.target);
-		var thisElement = event.target.className;
-		
-		if( !$('.date-picker-container').is(':visible') ){
+  $datePickerContainer.height(documentHeight);
+  $document.on("click", function(event) {
+    var target = $(event.target);
+    var thisElement = event.target.className;
 
-			if( thisElement == 'date'){
-				$('.date-picker-container').show();
-			}
-		}
+    if (!$datePickerContainer.is(':visible')) {
 
-		else{
-			if( thisElement == 'cancel'){
-				$('.date-picker-container').hide();
-			}
-		
-			else if( (thisElement == 'date-picker') || ( !target.parents('.date-picker').length)  ){
-				$('.date-picker-container').hide();	
-			}	
-		}
-		
-	});
+      if (thisElement == 'date') {
+        $datePickerContainer.show();
+      }
 
-	/* Date Picker */
-	$(".dates span").click(function(){
-		if ( !$( this ).hasClass( "inactive" ) ) {
-			$(".dates span").removeClass("selected");
-			$(this).addClass("selected");
-		}
-	});	
-	$(".dates span.selected").click(function(){
-		$(this).removeClass("selected");
-	});	
+    } else {
 
-	$(".time-container label").click( function(){
-		if( !$(this).prev("input:checkbox").is(':checked')){
-			$(this).prev("input:checkbox").prop('checked', true);
-		
-		}
-		else{
-			$(this).prev("input:checkbox").prop('checked', false);
-		}
-	});
+      if (thisElement == 'cancel') {
+        $datePickerContainer.hide();
+      } else if ((thisElement == 'date-picker') || (!target.parents('.date-picker').length)) {
+        $datePickerContainer.hide();
+      }
+
+    }
+
+  });
+
+  /* Date Picker */
+
+  var $datesSpan = $(".dates span");
+
+  $datesSpan.on("click", function() {
+    var $_this = $(this);
+    if (!$_this.hasClass("inactive")) {
+      $datesSpan.removeClass("selected");
+      $_this.addClass("selected");
+    }
+  });
+
+  $(".dates span.selected").on("click", function() {
+    var $_this = $(this);
+    $_this.removeClass("selected");
+  });
+
+  $(".time-container label").on("click", function() {
+    var $_this = $(this);
+    if (!$_this.prev("input:checkbox").is(':checked')) {
+      $_this.prev("input:checkbox").prop('checked', true);
+    } else {
+      $_this.prev("input:checkbox").prop('checked', false);
+    }
+  });
 
 
 });
