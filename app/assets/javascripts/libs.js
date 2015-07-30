d3bd713e533f5175d10f80f5531dd908818aa7c7
Essekia Paul <essekiarockz@gmail.com>
2015-07-30 23:59:28 +0530

    Basic structure of the timer. Needs work....

--- app/assets/javascripts/libs.js
+++ app/assets/javascripts/libs.js
@@ -1,5 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+//= require globalize
+//= require jquery-ui
 //= require bootstrap
 //= require twitter/typeahead
 //= require ddslick
+//= require moment
+//= require jquery.daterangepicker
--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -1,44 +1,98 @@
-$(document).ready(function() {
+$(document)
+  .ready(function() {
 
-  /* Pop up */
 
-  // $('#datePicker').modal('show');
+    /* Pop up */
+    var
+      documentHeight = $(document).height();
+    $(".popup-container").height(documentHeight);
 
-  $(".popup").on("click", '.close, .cancel', function() {
-    $(this).closest('.popup-container').hide();
-  });
+    // $('#datePicker').modal('show');
+    $(".popup").on("click", '.close, .cancel', function() {
+      $(this).closest('.popup-container').hide();
+    });
 
-  /* Date Picker */
-  var documentHeight = $(document).height();
-  $(".popup-container").height(documentHeight);
-  var $datesSpan = $(".dates span");
 
-  $datesSpan.on("click", function() {
-    var $_this = $(this);
-    if (!$_this.hasClass("inactive")) {
-      $datesSpan.removeClass("selected");
-      $_this.addClass("selected");
+    /* Date Picker */
+    $('#date-range').dateRangePicker({
+      startOfWeek: 'monday',
+      separator: ' ~ ',
+      format: 'DD.MM.YYYY HH:mm',
+      autoClose: false,
+      showShortcuts: false,
+    });
+
+    // var singleTimeInput = "<div class='time-selector'>"
+    //                     + "<label>Hour</label><div><span></span><input class='hours' type='text' name='hours' val=''/>" 
+    //                     + "<label>Minutes</label><input class='minutes' type='text' name='minutes' val=''/>" 
+    //                     + "<label></label><input class='am-pm' type='text name='am-pm' val=''/>";
+
+    var singleTimeInput = "<div class='time-selector'>"
+                        + "<input id='hours-spinner' value='08'>"
+                        + "<input id='minutes-spinner' value ='00'>"
+                        + "<span id='am-pm-spinner'></span>"
+                        + "</div>";
+
+    
+    $(".month-wrapper").after(singleTimeInput);
+     $( "#hours-spinner").spinner({
+      numberFormat: "d2",
+      spin: function( event, ui ) {
+        if ( ui.value > 11 ) {
+          
+          $( this ).spinner( "value", 0 );
+          return false;
+        } else if ( ui.value < 0 ) {
+          
+          $( this ).spinner( "value", 11 );
+          return false;
+        }
+      }
+     }); 
+    $( "#minutes-spinner").spinner({
+      // min: 0, 
+      // max: 60,
+      numberFormat: "d2",
+      spin: function( event, ui ) {
+        
+        if ( ui.value > 59 ) {
+          
+          $( this ).spinner( "value", 0 );
+          return false;
+        } else if ( ui.value < 0 ) {
+          
+          $( this ).spinner( "value", 59 );
+          return false;
+        }
+      }
+    }); 
+     
+     var dlist = ['am','pm']
+     $( "#am-pm-spinner" ).spinner({ 
+    create: function(){
+        $(this).parent().append('<input class="am-pm-spinner-text" value="'+dlist[0]+'">');
+    },
+    stop: function(event,ui) {    
+        $(this).siblings('.am-pm-spinner-text').val(dlist[$(this).val()]);
+    },
+    spin: function( event, ui ) {
+      if ( ui.value > 1 ) {
+          
+          $( this ).spinner( "value", 0 );
+          return false;
+        } else if ( ui.value < 0 ) {
+          
+          $( this ).spinner( "value", 1 );
+          return false;
+        }
     }
+
+ });
+
   });
 
-  $(".dates span.selected").on("click", function() {
-    var $_this = $(this);
-    $_this.removeClass("selected");
-  });
-
-  $(".time-container label").on("click", function() {
-    var $_this = $(this);
-    if (!$_this.prev("input:checkbox").is(':checked')) {
-      $_this.prev("input:checkbox").prop('checked', true);
-    } else {
-      $_this.prev("input:checkbox").prop('checked', false);
-    }
-  });
-
-});
 
 /* Password vs Confirm-Password Checking */
-
 function checkPassword() {
   var password = document.getElementById('password');
   var confirmPassword = document.getElementById('confirm-password');
