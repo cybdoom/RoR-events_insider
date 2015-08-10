1e244bedbb124d7b34be11bda5e586af328c6a77
pineapple-thief <gluhov1985@gmail.com>
2015-08-11 03:06:34 +1000

    Fixed couple of bugs

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+if Rails.env.development? || Rails.env.testing?
+  Dotenv::Railtie.load
+end
 
 require_relative '../lib/extensions/env_wrapper'
 
