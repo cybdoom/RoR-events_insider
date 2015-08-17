54cbe8641b5b0d5988030e093e9b320fc8281695
pineapple-thief <gluhov1985@gmail.com>
2015-08-17 23:26:36 +1000

    no message

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,4 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( libs.js )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
