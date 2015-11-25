ac400ad5d374fbfef74802e8e60ba4b9e4dfe114
cybdoom <cybdoom@gmail.com>
2015-11-25 18:02:28 +0200

    Created article model, fixed blog model

---
+++ app/models/article.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  blog_id    :integer
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Article < ActiveRecord::Base
+  belongs_to :blog
+  has_many :images, as: :imageable, dependent: :destroy
+end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,5 +10,6 @@
 #
 
 class Blog < ActiveRecord::Base
+  has_many :articles, dependent: :destroy
   has_many :images, as: :imageable, dependent: :destroy
 end
