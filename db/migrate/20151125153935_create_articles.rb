636caa0c3ec964c67f4e219a28eda2295d10082f
cybdoom <cybdoom@gmail.com>
2015-12-03 10:54:10 +0200

    Added author to article

--- db/migrate/20151125153935_create_articles.rb
+++ db/migrate/20151125153935_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.belongs_to :blog, foreign_key: true, index: true, null: false
       t.string :title, null: false
+      t.string :author
       t.text :body
       t.timestamps null: false
     end
