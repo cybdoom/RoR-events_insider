e09c9b43dce8bed59aa7e8023a3c21b555816178
cybdoom <cybdoom@gmail.com>
2015-12-17 11:51:03 +0200

    Fixed footer contact menu item, fixed auth for subscription

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  layout 'pages'
+  FREE_PAGE_SLUGS = [
+    'contact',
+    'getsubscription',
+    'alreadysub',
+    'about-johnny-monsarrat'
+  ]
 
+  layout 'pages'
+  skip_before_filter :authenticate_user!
+  before_action :check_authentication, only: [:show, :redirect_to_show]
   before_action :set_page, only: [:show, :redirect_to_show]
 
   def show
@@ -12,6 +20,10 @@ class PagesController < ApplicationController
   end
 
   private
+
+  def check_authentication
+    authenticate_user! unless FREE_PAGE_SLUGS.include? params[:page_slug]
+  end
 
   def set_page
     @page = Page.find_by_slug params[:page_slug]
